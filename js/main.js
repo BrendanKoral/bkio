a26cecd45b5a9871041e91a95548e6bf832da87d
Brendan Koral <hipposmelas@gmail.com>
2020-03-10 19:47:17 -0500

    Forgot to update the progress circle JS

--- js/main.js
+++ js/main.js
@@ -5,23 +5,17 @@
  ------------------------------------ 
  ====================================*/
 
-
-
 'use strict';
 
-
-$(window).on('load', function() { 
+$(window).on('load', function() {
 	/*------------------
 		Preloder
 	--------------------*/
-	$(".loader").fadeOut(); 
-	$("#preloder").delay(400).fadeOut("slow");
-
+	$('.loader').fadeOut();
+	$('#preloder').delay(400).fadeOut('slow');
 });
 
-
 (function($) {
-
 	/*------------------
 		Background set
 	--------------------*/
@@ -29,7 +23,6 @@ $(window).on('load', function() {
 		var bg = $(this).data('setbg');
 		$(this).css('background-image', 'url(' + bg + ')');
 	});
-
 
 	$('.review-slider').owlCarousel({
 		loop: true,
@@ -39,27 +32,25 @@ $(window).on('load', function() {
 		autoplay: true
 	});
 
-
-
 	$('.progress-bar-style').each(function() {
-		var progress = $(this).data("progress");
+		var progress = $(this).data('progress');
 		var prog_width = progress + '%';
 		if (progress <= 100) {
-			$(this).append('<div class="bar-inner" style="width:' + prog_width + '"><span>' + prog_width + '</span></div>');
-		}
-		else {
+			$(this).append(
+				'<div class="bar-inner" style="width:' + prog_width + '"><span>' + prog_width + '</span></div>'
+			);
+		} else {
 			$(this).append('<div class="bar-inner" style="width:100%"><span>' + prog_width + '</span></div>');
 		}
 	});
 
-
 	$('.lan-prog').each(function() {
-		var progress = $(this).data("lanprogesss");
-		var ele      = '<span></span>';
+		var progress = $(this).data('lanprogesss');
+		var ele = '<span></span>';
 		var ele_fade = '<span class="fade-ele"></span>';
-		
+
 		for (var i = 1; i <= 5; i++) {
-			if(i <= progress){
+			if (i <= progress) {
 				$(this).append(ele);
 			} else {
 				$(this).append(ele_fade);
@@ -67,74 +58,67 @@ $(window).on('load', function() {
 		}
 	});
 
-
 	/*------------------
 		Popup
 	--------------------*/
 	$('.portfolio-item .port-pic').magnificPopup({
 		type: 'image',
 		mainClass: 'img-popup-warp',
-		removalDelay: 500,
+		removalDelay: 500
 	});
 
+	if ($().circleProgress) {
+		//Set progress circle 1
+		$('#progress1').circleProgress({
+			value: 1,
+			size: 175,
+			thickness: 2,
+			fill: '#40424a',
+			emptyFill: 'rgba(0, 0, 0, 0)'
+		});
+		//Set progress circle 2
+		$('#progress2').circleProgress({
+			value: 0.9,
+			size: 175,
+			thickness: 2,
+			fill: '#40424a',
+			emptyFill: 'rgba(0, 0, 0, 0)'
+		});
 
+		//Set progress circle white
+		$('#progress3').circleProgress({
+			value: 0.75,
+			size: 175,
+			thickness: 2,
+			fill: '#ffffff',
+			emptyFill: 'rgba(0, 0, 0, 0)'
+		});
 
+		//Set progress circle white
+		$('#progress4').circleProgress({
+			value: 0.83,
+			size: 175,
+			thickness: 2,
+			fill: '#ffffff',
+			emptyFill: 'rgba(0, 0, 0, 0)'
+		});
 
-if($().circleProgress){
+		//Set progress circle skyblue
+		$('#progress5').circleProgress({
+			value: 0.75,
+			size: 175,
+			thickness: 2,
+			fill: '#009fff',
+			emptyFill: 'rgba(0, 0, 0, 0)'
+		});
 
-	//Set progress circle 1
-	$("#progress1").circleProgress({
-		value: 1,
-		size: 175,
-		thickness: 2,
-		fill: "#40424a",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-	//Set progress circle 2
-	$("#progress2").circleProgress({
-		value: 0.55,
-		size: 175,
-		thickness: 2,
-		fill: "#40424a",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-
-	//Set progress circle white
-	$("#progress3").circleProgress({
-		value: 0.75,
-		size: 175,
-		thickness: 2,
-		fill: "#ffffff",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-
-	//Set progress circle white
-	$("#progress4").circleProgress({
-		value: 0.83,
-		size: 175,
-		thickness: 2,
-		fill: "#ffffff",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-
-	//Set progress circle skyblue
-	$("#progress5").circleProgress({
-		value: 0.75,
-		size: 175,
-		thickness: 2,
-		fill: "#009fff",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-
-	//Set progress circle skyblue
-	$("#progress6").circleProgress({
-		value: 0.83,
-		size: 175,
-		thickness: 2,
-		fill: "#009fff",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-}
-
+		//Set progress circle skyblue
+		$('#progress6').circleProgress({
+			value: 0.83,
+			size: 175,
+			thickness: 2,
+			fill: '#009fff',
+			emptyFill: 'rgba(0, 0, 0, 0)'
+		});
+	}
 })(jQuery);
-
